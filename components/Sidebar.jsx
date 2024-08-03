b14a9bf0fb3c7ea81d5f184d0d9da224d3ca5a71
Nicholas-V <nickvasylyniuk@gmail.com>
2024-08-03 16:09:30 -0400

    adding theme toggle

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,7 +1,16 @@
+import SidebarHeader from "@/components/SidebarHeader";
+import NavLinks from "@/components/NavLinks";
+import MemberProfile from "@/components/MemberProfile";
+
 const Sidebar = () => {
     return (
-        <div className="text-6xl">
-            Sidebar
+        <div className="px-4 w-80 min-h-full bg-base-300 py-12 grid grid-rows-[auto,1fr,auto]">
+            {/* first row */}
+            <SidebarHeader/>
+            {/* second row */}
+            <NavLinks/>
+            {/* third row */}
+            <MemberProfile/>
         </div>
     );
 };
